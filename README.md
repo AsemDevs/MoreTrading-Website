3f342e3957b3849021095fcc0f29f21b5107d519
Asem Hamdi <asemhamdi97@gmail.com>
2023-06-02 15:30:40 +0300

    Install dockerized wordpress

---
+++ README.md
@@ -0,0 +1,1 @@
+# Landing Page More Trading Website
---
+++ wp-content/themes/more/index.php
@@ -0,0 +1,5 @@
+<?php
+/*
+* The main template file, it serves as the default template for any page that does not have a template file assigned to it.
+* This file is part of the 'More' theme by Asem Abdou.
+*/
