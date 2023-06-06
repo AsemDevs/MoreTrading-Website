ed4990ed1f567b23b18da7561aa5130511f1863c
Asem Hamdi <asemhamdi97@gmail.com>
2023-06-06 21:06:34 +0300

    Used Jquery for menus

--- wp-content/themes/more/assets/js/script.js
+++ wp-content/themes/more/assets/js/script.js
@@ -1,79 +1,63 @@
 jQuery(document).ready(function ($) {
-    $('.flex.flex-col.text-white').each(function () {
+    // Dropdown functionality for desktop #menu
+    $('#menu .menu-item-has-children').hover(function () {
+        $(this).find('.submenu').first().stop(true, true).slideDown();
+    }, function () {
+        $(this).find('.submenu').first().stop(true, true).slideUp();
+    });
+
+    // Toggle functionality for menuToggle button
+    $('#menu-toggle').on('click', function () {
+        $('#menu-mobile').toggleClass('show');
+    });
+
+    // Adding down arrows and toggle functionality for top-level menu items
+    var $menuItems = $('li.menu-item-has-children').filter(function() {
+        return !$(this).parents('.submenu').length;  // only keep top-level menu items
+    });
+
+    $menuItems.each(function () {
+        var $menuItem = $(this);
+        var $arrowElement = $('<span class="down-arrow"></span>');
+
+        if ($menuItem.closest('#menu-mobile').length) {
+            $menuItem.find('> a').after($arrowElement);
+        } else if ($menuItem.closest('#menu').length) {
+            $menuItem.find('> a').append($arrowElement);
+        }
+
+        // Toggle the correct submenu on click
+        $arrowElement.on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var $submenu = $menuItem.children('.submenu');
+            $submenu.slideToggle();
+        });
+    });
+
+$('.flex.flex-col.text-white').each(function () {
         var $menuSection = $(this);
         var $heading = $menuSection.find('h3');
         var $menu = $menuSection.find('ul');
         var $downArrow = $heading.find('.down-arrow');
 
-    
         // Toggle the menu on click
-        $downArrow.click(function () {
+        $downArrow.on('click', function () {
             $menu.slideToggle();
         });
     });
-    var menu = document.querySelector('#menu');
-    var menuToggle = document.getElementById('menu-toggle');
-    var navMenuMobile = document.getElementById('menu-mobile');
-    var menuItems = document.querySelectorAll('li.menu-item-has-children');
 
-    // Dropdown functionality for #menu
-    if (menu) {
-        var dropdowns = menu.querySelectorAll('.menu-item-has-children');
-        dropdowns.forEach(function (dropdown) {
-            dropdown.addEventListener('mouseover', function () {
-                this.querySelector('.submenu').style.display = 'block';
-            });
-            dropdown.addEventListener('mouseout', function () {
-                this.querySelector('.submenu').style.display = 'none';
-            });
-        });
-    }
+    // Menu toggle for mobile menu
+    $('.nav-menu-mobile').each(function () {
+        var $menuSection = $(this);
+        var $heading = $menuSection.find('.menu-item-has-children > a');
+        var $submenu = $heading.siblings('.submenu');
+        var $downArrow = $heading.find('.down-arrow');
 
-    // Toggle functionality for menuToggle button
-    if (menuToggle && navMenuMobile) {
-        menuToggle.addEventListener('click', function () {
-            navMenuMobile.classList.toggle('show');
-        });
-    }
-
-    // Adding down arrows and toggle functionality for each menu item
-    menuItems.forEach(function (menuItem) {
-        var arrowElement = document.createElement('span');
-        arrowElement.classList.add('down-arrow');
-
-        if (menuItem.closest('#menu-mobile')) {
-            menuItem.querySelector('a').after(arrowElement);
-        } else if (menuItem.closest('#menu')) {
-            menuItem.querySelector('a').appendChild(arrowElement);
-        }
-
-        arrowElement.addEventListener('click', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            var submenu = this.nextElementSibling;
-            if (submenu.style.display === 'none' || submenu.style.display === '') {
-                submenu.style.display = 'block';
-            } else {
-                submenu.style.display = 'none';
-            }
+        // Toggle the menu on click
+        $downArrow.on('click', function () {
+            $submenu.slideToggle();
         });
     });
-
-    var downArrows = document.querySelectorAll('li.menu-item-has-children .down-arrow');
-    downArrows.forEach(function (arrow) {
-        arrow.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            var submenu = this.nextElementSibling;
-            var parentMenuItem = this.parentElement.querySelector('a');
-
-            if (submenu.style.display === 'block') {
-                parentMenuItem.classList.add('primary-color');
-            } else {
-                parentMenuItem.classList.remove('primary-color');
-            }
-        });
-    });
-
 });
